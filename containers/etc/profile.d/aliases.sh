54586630cffe8140c7fa6464918a49abfdcf0293
Mike Glenn <mike.glenn@rammount.com>
2020-09-03 17:40:45 -0400

    initial import

---
+++ containers/etc/profile.d/aliases.sh
@@ -0,0 +1,4 @@
+  echo "alias l='ls -lha'" >> ~/.bashrc && \
+  echo "alias ll='ls -lha'" >> ~/.bashrc && \
+  echo "alias la='ls -A'" >> ~/.bashrc && \
+  echo "alias cls='clear && printf '\''\e[3J'\'''" >> ~/.bashrc 
